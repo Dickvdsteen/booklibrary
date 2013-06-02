d9e8c2eba8e0766294da1a41516b21345cdd40f2
Dick <dickvandersteen@gmail.com>
2013-06-02 20:06:29 +0200

    Added bootstrap

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Book.create
+
+Book.create
+
+Book.create
+
+Book.create
+Book.create
+Book.create
+Book.create
+Book.create
+Book.create
+
+@books = Book.all
+@books.each do |book|
+	book.image_link = "http://books.google.com/books?id=gfg13CM_kU8C&pg=PP1&img=1&zoom=5&sig=LsTwGVAsy_qWYMPM6HVDTPAMokg"
+	book.save
+end
